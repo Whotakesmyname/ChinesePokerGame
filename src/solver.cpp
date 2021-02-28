42119b86ec6014059e43fe2f5e4ca23c3a7a10f9
Harrison Chen <darkray@126.com>
2021-03-01 04:50:38 +0800

    [failure]Bitset cannot minus
    Signed-off-by: Harrison Chen <darkray@126.com>

--- src/solver.cpp
+++ src/solver.cpp
@@ -13,6 +13,7 @@
 #include "hand.h"
 #include "pool.h"
 #include "utils.h"
+#include "thread_pool.h"
 
 // 牌副数, 每副54张
 constexpr size_t CARD_PACKS_N = 1;
@@ -21,6 +22,32 @@ constexpr size_t CARD_PACKS_N = 1;
 class Solver {
     using hand_t = Hand<CARD_PACKS_N>;
     using pattern_t = Pattern<CARD_PACKS_N>;
+
+    struct Node
+    {
+        int layer;
+        std::shared_ptr<pattern_t> last_move;
+        signed char score;
+        std::shared_ptr<hand_t> lord_hand;
+        std::shared_ptr<hand_t> farmer_hand;
+        std::list<std::unique_ptr<Node>> children;
+    };
+    using NodePtr = std::unique_ptr<Node>;
+
+public:
+    Solver(std::vector<Card>&& lord, std::vector<Card>&& farmer) {
+        hand_t lord_hand = hand_t()
+    }
+    
+private:
+    std::vector<std::pair<Card, int>> parse_cards(std::vector<Card>&& cards) const {
+        
+    }
+
+    NodePtr root_;
+    Pool<Hand, CARD_PACKS_N> hand_pool_;
+    Pool<Pattern, CARD_PACKS_N> pattern_pool_;
+    inline static ThreadPool thread_pool_ = ThreadPool(std::thread::hardware_concurrency() - 1);
 };
 
 int main(int argc, char const *argv[])
